b4d05e08c4a55feb4e13c296307cc0fff2aa6b7d
temi <temitopem@gmail.com>
2019-06-27 16:33:03 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        Random rand = new Random();
+        int color;
+        String evenorOdd;
+        String highLow;
+        boolean flag = true;
+        userInput = userInput.toLowerCase();
+
+
+
+
+        while (flag)
+        {
+
+
+
+            System.out.println("\"Enter the type of bet you would like to place (red|black|even|odd|high|low):"
+            );
+            if (userInput)
+        }
+
+    }
+
+}
